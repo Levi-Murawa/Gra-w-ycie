7c76c5fcedc1e9e17eea0ac1133e7bc195fbc782
Levi-Murawa <60005634+Levi-Murawa@users.noreply.github.com>
2022-11-09 12:55:24 +0100

    Funkcja animacja - w pełni działająca gra
    Funkcja gra została zdebugowana i już działa poprawnie.
    Dodałem funkcję animacja która umożliwia łatwe wywołanie animacji przebiegu gry

--- Gra_w_zycie.py
+++ Gra_w_zycie.py
@@ -2,6 +2,10 @@ from matplotlib import pyplot as plt
 import random
 import time
 
+tab_zolw = [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]]
+tab_blink = [[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]]
+tab_blink_maly = [[0,0,0],[1,1,1],[0,0,0]]
+
 def zrob_tablice(szer, wys): ##funckja tworząca tablicę w sposób losowy
     data = []
     for i in range(wys):
@@ -10,8 +14,8 @@ def zrob_tablice(szer, wys): ##funckja tworząca tablicę w sposób losowy
 
 
 def gra(stan):
-    badanie = []
-    wi_badanie = []
+    wier = []
+    tablica = []
 
     maks_wiersze = len(stan)
     maks_kolumny = len(stan[0])
@@ -19,6 +23,7 @@ def gra(stan):
 
         for kol in range(maks_kolumny):
             a = 0
+
             if (kol>0):
                 a = a + stan[wi][kol-1]
             if (kol+1<maks_kolumny):
@@ -27,25 +32,42 @@ def gra(stan):
                 a = a + stan[wi-1][kol]
             if (wi+1<maks_wiersze):
                 a = a + stan[wi+1][kol]
-            if (a == 2 or a == 3):
-                stan[wi][kol] = 1
-            else:
-                stan[wi][kol] = 0
+
+            if (wi>0 and kol>0):
+                a = a + stan[wi-1][kol-1]
+            if (wi+1<maks_wiersze and kol+1< maks_kolumny):
+                a = a + stan[wi+1][kol+1]
+            if (wi+1 < maks_wiersze and kol > 0):
+                a = a + stan[wi + 1][kol - 1]
+            if (wi>0 and kol+1 < maks_kolumny):
+                a = a + stan[wi - 1][kol + 1]
+            ##print('A równa sie:',a, 'Wartosc rowna sie', stan[wi][kol]) ##wypis w celu debugowania
+            if stan[wi][kol] == 1: ##żywa komurka będzie żyć przy dwóch lub trzeh sąsiadach, innaczej umiera
+                if (a == 2 or a == 3):
+                    wier.append(1)
+                else:
+                    wier.append(0)
+            else: ##jeśli komurka jest martwa, to odrodiz się przy trzech sąsiadach
+                if a == 3:
+                    wier.append(1)
+                else:
+                    wier.append(0)
+        tablica.append(wier)
+        wier = []
+    return tablica
+
+def animacja(tab_wejsciowa, ile_okr, czas_okr):
+    tab_copy = tab_wejsciowa.copy()
+    plt.ion()
+    for i in range(ile_okr):
+        plt.imshow(tab_copy)
+        plt.pause(czas_okr)
+        tab_copy = gra(tab_copy)
+        plt.clf()
 
 
-
-
-plt.ion()
-x = 4
-y = 4
+x = 200
+y = 200
 
 dat = zrob_tablice(x, y)
-
-
-
-for i in range(10):
-    plt.imshow(dat)
-    plt.pause(2)
-    gra(dat)
-    plt.clf()
-
+animacja(dat, 100, 0.01)
